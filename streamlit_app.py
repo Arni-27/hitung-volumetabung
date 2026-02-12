65f0d099db58edd12f9c83eca5562a0409952965
Arni-27 <arnialus@gmail.com>
2026-02-12 19:26:59 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,32 +2,8 @@
 
 import streamlit as st
 
-st.title("Kalkulator BMI")
+st.title("Kalkulator hitung volume tabung")
 
-# Input
-nama = st.text_input("Masukkan Nama")
-berat = st.number_input("Masukkan Berat Badan (kg)", min_value=1.0)
-tinggi = st.number_input("Masukkan Tinggi Badan (cm)", min_value=50.0)
-
-# Tombol hitung
-if st.button("Hitung BMI"):
-    tinggi_m = tinggi / 100  # cm ke meter
-    bmi = berat / (tinggi_m ** 2)
-
-    # Kategori BMI
-    if bmi < 18.5:
-        kategori = "Kurus"
-    elif bmi < 25:
-        kategori = "Normal"
-    elif bmi < 30:
-        kategori = "Gemuk"
-    else:
-        kategori = "Obesitas"
-
-    st.success(f"Halo {nama}, BMI kamu adalah **{bmi:.2f}**")
-    st.info(f"Kategori: **{kategori}**")
-
-        
 
    
 
